8250526bcc03162720c4e4e2c91919312f50669d
orchsik <orchsik@naver.com>
2021-12-13 17:55:56 +0900

    basic setting

---
+++ __tests__/App.test.tsx
@@ -0,0 +1,8 @@
+import 'react-native';
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import App from '../App';
+
+it('renders correctly', () => {
+  render(<App />);
+});
