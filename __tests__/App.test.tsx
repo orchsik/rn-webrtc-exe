d9f36455e0c1bd6cbbde549702b53d2fba330525
orchsik <orchsik@naver.com>
2021-12-14 13:10:26 +0900

    Add: 모듈(react-native-vector-icon)

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,7 +1,7 @@
 import 'react-native';
 import React from 'react';
 import { render } from '@testing-library/react-native';
-import App from '../App';
+import App from '../src/App';
 
 it('renders correctly', () => {
   render(<App />);
